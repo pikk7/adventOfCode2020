ef6f634f2f7a929c2ad68727e77715696be37506
unknown <durza13@gmail.com>
2020-12-22 19:01:10 +0100

    9es elfő fele

---
+++ 9/main.py
@@ -0,0 +1,53 @@
+import os 
+import string
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+fo = open(dir_path + '/' + 'input.txt', "r+")
+inputs = fo.readlines()
+inputs= [int(item.strip()) for item in inputs]
+fo.close()
+
+#first part:
+
+def is_prembeable(index_of_number,array,length):
+  """
+  CHeck hogy jo e a szam
+  """
+  i=index_of_number-length
+  
+  while i<index_of_number:
+    j=i+1
+    while j<index_of_number:
+      if(array[index_of_number]==array[i]+array[j]):
+        return True
+      j=j+1
+    i=i+1
+
+  return False
+
+
+def bad_number(inputs,index):
+  while index<len(inputs):
+    c=inputs[index]
+    if not(is_prembeable(index,inputs,25)):
+      return int(c)
+    index+=1
+
+#print(bad_number(inputs,5))
+
+#second part:
+
+def search_numbers(array):
+  i=0
+  while i<len(array):
+    j=0
+    while j<len(array):
+      #print(sum(array[i:j]))
+      if(int(sum(array[i:j]))==bad_number(inputs,25)):
+        return array[i:j]
+      j=j+1
+    i=i+1
+  return -1
+  
+print(max(search_numbers(inputs)))
+print(min(search_numbers(inputs)))
